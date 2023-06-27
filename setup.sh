5a44df03d43a628a517d842575c33906859b47ff
Shengjie Lin <StArchon@outlook.com>
2023-06-26 22:38:13 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 # NOTE: this setup script will be executed right before the launcher file inside the container,
 #       use it to configure your environment.
 
-source ${CPK_CODE_DIR}/devel/setup.bash
+. ${CPK_CODE_DIR}/devel/setup.bash
 
 export ROS_HOSTNAME=$(hostname).local
